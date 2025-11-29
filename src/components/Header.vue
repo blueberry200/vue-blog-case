98c56fc0f39d932a0c8ba253e19a51763f848545
blueberry200 <blueberry4006@gmail.com>
2025-11-29 21:09:17 +0800

    t2

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <div
     class="sticky flex justify-between items-center w-full h-20 px-10 bg-green-700 z-100"
   >
-    <div class="text-3xl font-black text-white">Vue案例展示t1</div>
+    <div class="text-3xl font-black text-white">Vue案例展示t2</div>
     <div
       class="text-xl font-black text-white hover:cursor-pointer"
       @click="router.push('/')"
